0ba73d2c3a346b14ca40d8408bfd201266e5c1c8
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2023-07-26 09:19:59 +0200

    dev4

--- components/MapBox.js
+++ components/MapBox.js
@@ -1,11 +1,23 @@
 import {useState} from 'react';
 import {View} from 'react-native';
 import MapboxGL from '@rnmapbox/maps';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Image,
+  useColorScheme,
+  FlatList,
+  Pressable,
+  Dimensions,
+} from 'react-native';
 MapboxGL.setAccessToken(
   'sk.eyJ1IjoibmJvYmljMSIsImEiOiJjbGtneWhwZTMwNmF1M2Vxamdta2pycjc3In0.jGEohkSjIlS-UP08gbLBhA',
 );
 
-const App = () => {
+const MapBox = () => {
   const [coordinates] = useState([18.410107299618403, 43.85754735842143]);
   const [scc] = useState([18.40807197932288, 43.85553119082344]);
   const [bbi] = useState([18.415763452554405, 43.85876456886843]);
@@ -23,3 +35,38 @@ const App = () => {
     </View>
   );
 };
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+    flex: 1,
+  },
+  markerContainer: {
+    alignItems: 'center',
+    width: 60,
+    backgroundColor: 'transparent',
+    height: 70,
+  },
+  map: {
+    flex: 1,
+  },
+  textContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    paddingHorizontal: 5,
+    flex: 1,
+  },
+  icon: {
+    paddingTop: 10,
+  },
+});
+export default MapBox;
